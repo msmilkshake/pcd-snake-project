e1962dfe4b542d21cc431db236351ba9d5eb95ca
msmilkshake <prnfilipe@gmail.com>
2023-11-08 21:51:11 +0000

    blocking queue

--- src/util/BlockingQueue.java
+++ src/util/BlockingQueue.java
@@ -1,4 +1,44 @@
 package util;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BlockingQueue<E> {
+    private Queue<E> queue = new LinkedList<>();
+    private int capacity = 0;
+
+    public BlockingQueue() {
+    }
+
+    public BlockingQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public synchronized void put(E element) throws InterruptedException {
+        while (capacity > 0 && queue.size() >= capacity) {
+            wait();
+        }
+        queue.add(element);
+        if (capacity > 0) {
+            notifyAll();
+        }
+    }
+
+    public synchronized E take() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait();
+        }
+        E head = queue.poll();
+        notifyAll();
+        return head;
+    }
+
+    public synchronized int size() {
+        return queue.size();
+    }
+
+    public synchronized void clear() {
+        queue.clear();
+        notifyAll();
+    }
 }
